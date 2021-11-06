454403d55a03d22bc95f5f06d75c6b2e88cdc81a
ultramar1ne <louqun97@outlook.com>
2021-11-06 13:11:13 -0700

    simple statistic about train set

--- feature_generator.py
+++ feature_generator.py
@@ -83,7 +83,7 @@ class FQDN:
         self.subdomain_characters = subdomain_characters_counter(fqdn)
         self.upperclass_letters_num = self.upperclass_letters_counter(fqdn)
         self.entropy = self.cal_entropy(fqdn)  # todo:有Bug!!:特殊符号不知道咋处理——找个entropy的库吧
-        self.class_0_1 = int(class_0_1)
+        self.class_0_1 = 0 if class_0_1=='0' else 1 #todo:他给出了不同隧道工具的分类
         self.csv=[self.name[:-1],self.class_0_1,self.max_label_legnth,self.avg_label_legnth,self.labels_num,self.digits_num,self.total_characters,self.upperclass_letters_num,self.entropy]
 
 
@@ -97,6 +97,21 @@ for item in reader:
 csvFile.close()
 
 new_feature = pd.DataFrame([ FQDNs[_].csv for _ in range(len(FQDNs))], columns=['FQDN','class_0_1','max_label_legnth','avg_label_legnth','labels_num','digits_num','total_characters','upperclass_letters_num','entropy'])
-new_feature.to_csv('new_features.csv',index=None)
+new_feature.to_csv('new_features.csv',index=None,encoding="utf8")
 
-print("new features successfully generated as in new_featrues.csv")
+print("new features successfully generated as in new_features.csv")
+
+csvFile = open("new_features.csv", "r", encoding="utf8")
+reader = csv.reader(csvFile)
+mal_counter,heal_counter=0,0
+for item in reader:
+    if item[1]=='0':
+        if heal_counter<5:
+            print("healthy",item[0])
+        heal_counter+=1
+    if item[1]=='1':
+        if mal_counter<5:
+            print("malicious",item[0])
+        mal_counter+=1
+csvFile.close()
+print("0:",heal_counter,"1:",mal_counter)
